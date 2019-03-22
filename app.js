3f50c06f45a6eb9234876ce5677082b438e5da4d
costanzopa <nitopatapufete11>
2019-03-22 15:08:49 -0300

    Removing notes Method

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
 const chalk    = require('chalk');
 const yargs    = require('yargs');
-const notes = require('./notes.js');
+const notes    = require('./notes.js');
 
 
-yargs.version('1.1.0');
+// Customize yargs version
+yargs.version('1.1.0')
 
+// Create add command
 yargs.command({
-   command: 'add',
-   describe: 'Add a new note',
-   builder: {
-     title: {
-         describe: 'Note title',
-         demandOption: true,
-         type: 'string'
-     },
-     body: {
-         describe: 'Note body',
-         demandOption: true,
-         type: 'string'
-     }
-   },
-   handler: function(argv) {
-      notes.add(argv.title, argv.body);
-   }
-});
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
 
 
+// Create remove command
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function() {
-        console.log('Removing a note.');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
-});
+})
 
 
 yargs.command({
--- notes.js
+++ notes.js
@@ -1,21 +1,38 @@
 const fs = require('fs');
+const chalk = require('chalk')
 
 
-const add = function (title, body) {
+const addNote = function (title, body) {
 	const  notes = loadNotes();
-	const  duplicatesNotes = notes.filter(function(note) {
+	const  duplicateNotes = notes.filter(function(note) {
 		return note.title === title
 	})
 
-	if (duplicatesNotes.length === 0) {
-			notes.push({
-			title: title,
-			body: body
-		});
-		saveNotes(notes);
-	}
+	if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
 };
 
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
 
 const loadNotes = function() {
 	try {
@@ -34,5 +51,6 @@ const saveNotes = function(notes) {
 }
 
 module.exports = {
-	add: add
+	addNote: addNote,
+	removeNote: removeNote
 };
