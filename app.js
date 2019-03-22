128c00c5c9d32daa9249047d973506803aaf40a9
costanzopa <nitopatapufete11>
2019-03-22 11:30:14 -0300

    First notes app

---
+++ app.js
@@ -0,0 +1,56 @@
+const chalk    = require('chalk');
+const yargs    = require('yargs');
+const getNotes = require('./notes.js');
+
+
+yargs.version('1.1.0');
+
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   builder: {
+     title: {
+         describe: 'Note title',
+         demandOption: true,
+         type: 'string'
+     },
+     body: {
+         describe: 'Note body',
+         demandOption: true,
+         type: 'string'
+     }
+   },
+   handler: function(argv) {
+       console.log('Title: ' + argv.title);
+       console.log('Title: ' + argv.body);
+   }
+});
+
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note.');
+    }
+});
+
+
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function() {
+        console.log('Listing out all notes.');
+    }
+});
+
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note.');
+    }
+});
+
+yargs.parse();
